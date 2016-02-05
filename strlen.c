84bcae6ccf0c03d6d7df4ecba585c7b6b31f1c2c
liamweiland <liamweiland@gmail.com>
2016-02-05 16:05:12 -0500

    working length of string program

--- strlen.c
+++ strlen.c
@@ -1,9 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+
 int stringlen(char string[]);
 
 int main(){
 
+  char str[] = "hello world";
+
+  printf("The length of the string \"%s\" is %i\n",str, stringlen(str));
+
+  return(0);
 }
 
 int stringlen(char string[]){
+  int length = 0;
 
+   while (string[length] != '\0')
+      length++;
+
+   return length;
 }
